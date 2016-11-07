5a3645fdb5fe1c81268dc91c76100354b53a9c2a
Cameron Fabbri <cameronfabbri@gmail.com>
2016-11-06 20:39:11 -0500

    added eval

---
+++ eval/eval_one.py
@@ -0,0 +1,62 @@
+"""
+
+Cameron Fabbri
+
+Evaluation by just looking at the original image and the resulting image from the network
+
+"""
+
+import tensorflow as tf
+import cv2
+import sys
+import numpy as np
+
+sys.path.insert(0, '../utils/')
+sys.path.insert(0, '../model/')
+
+import architecture
+
+def eval(checkpoint_dir, image):
+   with tf.Graph().as_default() as graph:
+
+      input_image = tf.placeholder(tf.float32, shape=(1,144,160,3))
+      logit = architecture.inference(input_image, "test")
+
+      variables = tf.all_variables()
+      init      = tf.initialize_all_variables()
+      sess      = tf.Session()
+      saver     = tf.train.Saver(variables)
+
+      tf.train.start_queue_runners(sess=sess)
+
+      ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+
+      if ckpt and ckpt.model_checkpoint_path:
+         print 'Restoring model...'
+         try:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+         except:
+            print 'Could not restore model'
+            raise
+            exit()
+
+      graph_def = sess.graph.as_graph_def(add_shapes=True)
+      
+      img = cv2.imread(image)
+      img = img.astype('float')
+
+      fake = np.zeros((1,144,160,3))
+      fake[0,:,:,:] = img
+
+      gen_img = sess.run([logit], feed_dict={input_image:fake})[0]
+
+      cv2.imwrite('output.png', gen_img[0,:,:,:])
+
+def main(argv=None):
+   eval(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+   if len(sys.argv) < 2:
+      print "Usage: python eval.py /path/to/model/ image.png"
+      exit()
+   tf.app.run()
