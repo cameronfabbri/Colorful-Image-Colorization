c1631b08b7df1f9cf1690c18974a4937ee450b79
Cameron Fabbri <cameronfabbri@gmail.com>
2016-11-10 20:45:20 -0500

    added comments

--- utils/extract_frames.sh
+++ utils/extract_frames.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Script for extracting frames from multiple video files
+# If you have a folder filled with videos like so...
+#
+# folder/
+#    - video_1.mp4
+#    - video_2.mp4
+#    - ...
+
+# you can simply run the script by doing `./extract_frames.sh folder/
+# and it will create an individual folder for each video and place the
+# images in their respective folders.
+
 for file in "$1"/*.*; do
    destination="${file%.*}"
    echo "Extracting from $file..."
